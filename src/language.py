5058e4071cdac7a7b517d0db0c9d1bc48990bc7e
Harvey Barnhard <harveybarnhard@gmail.com>
2021-01-01 20:59:25 -0500

    Fake language data

--- src/language.py
+++ src/language.py
@@ -1,12 +1,20 @@
 import duolingo
 import json
 import os
+import pandas as pd
+from datetime import datetime
 
 # Sign in and get daily xp progresss
 lingo = duolingo.Duolingo('HarveyBarn', os.getenv('DUOLINGO'))
 response = lingo.get_daily_xp_progress()
 # Get number of xp today
 xp = response['xp_today']
+
 # Get date of first lesson
-date = response['lessons_today'][0]['time']
-#lingo.get_language_progress('de')
+date_unix = response['lessons_today'][0]['time']
+date_time = datetime.fromtimestamp(date_unix)
+date_string = date_time.strftime('%m-%d-%Y')
+
+df = pd.read_csv('https://raw.githubusercontent.com/harveybarnhard/personal-goals/main/data/language.csv')
+
+print(df)
