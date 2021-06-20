b3637abf2822784a1cfd67b68592979c01d165e8
Harvey Barnhard <harveybarnhard@gmail.com>
2021-06-20 11:55:25 -0400

    Code will now run even if no Duolingo lesson on a given day

--- src/language.py
+++ src/language.py
@@ -11,32 +11,33 @@ print(response)
 # Get number of xp today
 xp = response['xp_today']
 
-# Get date of first lesson today
-date_unix = response['lessons_today'][0]['time']
-date_time = datetime.fromtimestamp(date_unix).date()
-date_string = date_time.strftime('%m-%d-%Y')
-df = pd.read_csv('https://raw.githubusercontent.com/harveybarnhard/personal-goals/main/data/raw/language.csv')
+# Get date of first lesson today, if any lessons were performed
+if len(response['lessons_today']) > 0 :
+    date_unix = response['lessons_today'][0]['time']
+    date_time = datetime.fromtimestamp(date_unix).date()
+    date_string = date_time.strftime('%m-%d-%Y')
+    df = pd.read_csv('https://raw.githubusercontent.com/harveybarnhard/personal-goals/main/data/raw/language.csv')
 
-# Add a row
-df['timestamp'] = df['timestamp'].astype(int)
-df['temp'] = df['timestamp'].apply(lambda x: datetime.fromtimestamp(x).date())
-max_date = df.temp.max()
-if date_time >= max_date:
-    if date_time == max_date:
-        del df['temp']
-        df.loc[df.index.max()] = ['German'] + [date_string] + [xp] + [date_unix]
-    else:
-        del df['temp']
-        df.loc[df.index.max() + 1] = ['German'] + [date_string] + [xp] + [date_unix]
-df.to_csv('./data/raw/language.csv', index=False)
+    # Add a row
+    df['timestamp'] = df['timestamp'].astype(int)
+    df['temp'] = df['timestamp'].apply(lambda x: datetime.fromtimestamp(x).date())
+    max_date = df.temp.max()
+    if date_time >= max_date:
+        if date_time == max_date:
+            del df['temp']
+            df.loc[df.index.max()] = ['German'] + [date_string] + [xp] + [date_unix]
+        else:
+            del df['temp']
+            df.loc[df.index.max() + 1] = ['German'] + [date_string] + [xp] + [date_unix]
+    df.to_csv('./data/raw/language.csv', index=False)
 
-# Calculate first monday of week
-df['date'] = pd.to_datetime(df['date'])
-df['monday'] = df['date'] -  pd.to_timedelta(arg=df['date'].dt.weekday, unit='D')
+    # Calculate first monday of week
+    df['date'] = pd.to_datetime(df['date'])
+    df['monday'] = df['date'] -  pd.to_timedelta(arg=df['date'].dt.weekday, unit='D')
 
-# collapse by week and language
-df = df.groupby(['monday'], as_index=False).agg({
-    'xp':'sum'
-})
-df.rename(columns={'xp':'duolingo_xp'}, inplace=True)
-df.to_csv('./data/language.csv', index=False)
+    # collapse by week and language
+    df = df.groupby(['monday'], as_index=False).agg({
+        'xp':'sum'
+    })
+    df.rename(columns={'xp':'duolingo_xp'}, inplace=True)
+    df.to_csv('./data/language.csv', index=False)
